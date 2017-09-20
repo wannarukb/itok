ed56be8eaf19cf153e99c44d6b84c50b4f9810de
home <cmmadnat@gmail.com>
2017-09-20 10:59:17 +0700

    add area service

---
+++ src/main/kotlin/com/orbit/itok/service/AreaService.kt
@@ -0,0 +1,22 @@
+package com.orbit.itok.service
+
+import com.googlecode.objectify.annotation.Entity
+import com.googlecode.objectify.annotation.Id
+import org.springframework.stereotype.Service
+
+/**
+ * Created For RIDMIS Web service
+ */
+@Entity
+data class MemberLand(@Id var id: Long? = null, var landOrder: Int? = 0,
+                      var address: Address = Address(), var lat: Float? = null, var lng: Float? = null,
+                      var basin: String? = null, var posessionType: String? = null, var posessionDocument: String? = null,
+                      var rai: Float? = null, var gnan: Float? = null, var wah: Float? = null, var intendedPurpose: String? = null,
+                      var usage: String? = null, var characteristic: String? = null, var soilType: String? = null, var problem: String? = null)
+
+interface MemberLandService
+
+@Service
+class MemberLandServiceImpl : MemberLandService {
+
+}
