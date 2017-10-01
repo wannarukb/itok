fc48af1d393fa9f420eeb5e46b7a61407c360b34
cmmadnat <cmmadnat@gmail.com>
2017-10-02 02:23:44 +0700

    fix the bug with uploading

--- src/main/resources/static/js/land.js
+++ src/main/resources/static/js/land.js
@@ -1,18 +1,38 @@
 $(function () {
     var pageLoad = function () {
 
-        Dropzone.options.myAwesomeDropzone = false;
-        $('#my-awesome-dropzone').dropzone();
-        var getAction = function () {
-            $.ajax({
-                url: '/land/' + id + '/action',
-                success: function (data) {
-                    $('#my-awesome-dropzone').attr('action', data)
+        // Dropzone.options.myAwesomeDropzone = false;
+        // $('#my-awesome-dropzone').dropzone();
+        Dropzone.options.myAwesomeDropzone = {
+            init: function () {
+                var dropzone = this.options;
+
+                var aj = function () {
+                    $.ajax({
+                        url: '/land/' + id + '/action',
+                        success: function (data) {
+                            dropzone.url = data;
+                        }
+                    });
                 }
-            })
+                aj();
+                this.on("processing", function (file) {
+                    aj();
+                });
+            }
         };
-        getAction();
-        setInterval(getAction, 5 * 60 * 1000)
+        // var getAction = function () {
+        //     $.ajax({
+        //         url: '/land/' + id + '/action',
+        //         success: function (data) {
+        //             // $('#my-awesome-dropzone').attr('action', data)
+        //
+        //
+        //         }
+        //     })
+        // };
+        // getAction();
+        // setInterval(getAction, 5 * 60 * 1000)
     }
     pageLoad();
     SingApp.onPageLoad(pageLoad);
