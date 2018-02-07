0b4c0b8af585acd30f79fd586c36ca3e388dc4e4
home <cmmadnat@gmail.com>
2018-02-07 15:29:11 +0700

    fix router

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -6,11 +6,19 @@ import {Provider} from "react-redux";
 import {createStore} from "redux";
 import member from './duck/member'
 import MemberListComponent from './components/member-list-component'
+import MemberViewComponent from './components/member-view-component'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-// let store =createStore(member)
+
+let store =createStore(member)
 
 
 ReactDOM.render(
-    <MemberListComponent/>,
+    <Provider store={store}>
+        <Router>
+            <Route path="/member" component={MemberListComponent}/>
+            <Route path="/member/view" component={MemberViewComponent}/>
+        </Router>
+    </Provider>,
     document.getElementById('memberApp')
 )
